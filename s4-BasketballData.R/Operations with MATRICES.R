e31c27c65488b252d7c1aa02e116b0beb7e92fb3
Tim W <bigwillt375@outlook.com>
2018-08-30 20:33:38 -0500

    New Commit of SDS R Course

---
+++ s4-BasketballData.R/Operations with MATRICES.R
@@ -0,0 +1,13 @@
+Games
+rownames(Games)
+colnames(Games)
+Games["LeBronJames","2012"]
+
+FieldGoals
+round(FieldGoals / Games,1)
+
+MinutesPlayed / Games
+round(MinutesPlayed /Games)
+
+FieldGoals / FieldGoalAttempts
+round(FieldGoals/FieldGoalAttempts,2)
